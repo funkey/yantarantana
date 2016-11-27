296ea39f73f2f5c74488277a2a37b78fe6945230
Jan Funke <jfunke@iri.upc.edu>
2016-11-27 22:47:30 +0100

    add toy touch event agent

--- yantarantana.cpp
+++ yantarantana.cpp
@@ -8,7 +8,11 @@
 class TestView : public sg::Agent<
 		TestView,
 		sg::Accepts<
-			sg_gui::Draw
+			sg_gui::Draw,
+			sg_gui::PenDown,
+			sg_gui::PenUp,
+			sg_gui::FingerDown,
+			sg_gui::FingerUp
 		>
 >{
 
@@ -27,6 +31,26 @@ public:
 		glVertex2f(100, 100);
 		glEnd();
 	}
+
+	void onSignal(sg_gui::PenDown& signal) {
+
+		std::cout << "pen down at " << signal.ray << ", button " << signal.button << std::endl;
+	}
+
+	void onSignal(sg_gui::PenUp& signal) {
+
+		std::cout << "pen up at " << signal.ray << ", button " << signal.button << std::endl;
+	}
+
+	void onSignal(sg_gui::FingerDown& signal) {
+
+		std::cout << "finger " << signal.id << " down at " << signal.ray << std::endl;
+	}
+
+	void onSignal(sg_gui::FingerUp& signal) {
+
+		std::cout << "finger " << signal.id << " up at " << signal.ray << std::endl;
+	}
 };
 
 int main(int argc, char** argv) {
@@ -36,7 +60,9 @@ int main(int argc, char** argv) {
 		util::ProgramOptions::init(argc, argv);
 		logger::LogManager::init();
 
-		auto window = std::make_shared<sg_gui::Window>("yantarantana");
+		sg_gui::WindowMode mode;
+		mode.fullscreen = true;
+		auto window = std::make_shared<sg_gui::Window>("yantarantana", mode);
 		auto test = std::make_shared<TestView>();
 
 		window->add(test);
